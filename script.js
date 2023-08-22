8b22e256c180d9da68d61abcbe1e1af957cc076c
parakh.pixel6 <parakh@pixel6.co>
2023-08-22 17:27:24 +0530

    Sass updated history built

--- script.js
+++ script.js
@@ -15,3 +15,49 @@ window.onload = function () {
         }
     }, 200); // Adjust the interval as needed
 };
+
+
+
+//Navbar on different section
+
+const history = document.getElementById("history");
+const navbar = document.querySelector(".navbar");
+const menuItems = document.querySelectorAll(".menu-item");
+const companyName = document.querySelector(".company-name");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    // When the second section is at the top
+    if (entry.target.id === "history" && entry.isIntersecting) {
+        navbar.style.backgroundColor = "#fff"; // Change this color as needed
+        menuItems.forEach(menuItem => {
+            menuItem.style.color = "#000"; // Change this color as needed
+            menuItem.style.borderBottom = "1px solid #000"; // Change this color as needed
+        });
+        companyName.style.display="block";
+    } else {
+        menuItems.forEach(menuItem => {
+            menuItem.style.borderBottom = "1px solid #fff"; // Change this color as needed
+            menuItem.style.color = "#fff"; // Change this color as needed
+        })
+        companyName.style.display="none";
+        navbar.style.backgroundColor = "transparent";
+    }
+  });
+}, { threshold: 1 });
+
+observer.observe(history);
+
+
+
+//Swiper JS
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
